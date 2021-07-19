eb9d40ef1480a8554805ddea9567395f0ed406b0
Gui-dev <adrianon10@gmail.com>
2021-07-19 03:54:42 -0300

    feat: add component ListDivider

---
+++ src/components/ListDivider/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native'
+
+import { colors } from '../../assets/styles/global'
+
+export const Container = styled.View`
+  align-self: flex-end;
+  height: 1px;
+  width: 80%;
+  background-color: ${colors.secondary40};
+  margin: 21px 0;
+`
