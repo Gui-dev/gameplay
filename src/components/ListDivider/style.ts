1e8119ddbea2a8ca25b53586a7e02208cc9ce285
Gui-dev <adrianon10@gmail.com>
2021-07-20 03:35:13 -0300

    style: add margin-top and margin-bottom

--- src/components/ListDivider/style.ts
+++ src/components/ListDivider/style.ts
@@ -5,7 +5,8 @@ import { colors } from '../../assets/styles/global'
 export const Container = styled.View`
   align-self: flex-end;
   height: 1px;
-  width: 80%;
+  width: 78%;
   background-color: ${colors.secondary40};
-  margin: 21px 0;
+  margin-top: 2px;
+  margin-bottom: 31px;
 `
