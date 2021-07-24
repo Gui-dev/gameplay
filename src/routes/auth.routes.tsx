89150d37dd79bf0dc78b2441884f734cdb8d459a
Gui-dev <adrianon10@gmail.com>
2021-07-24 03:45:25 -0300

    feat: add new routes, AppointmentDetails and AppointmentCreate

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,13 +3,15 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import { SignIn } from '../screens/SignIn'
 import { Home } from '../screens/Home'
+import { AppointmentDetails } from './../screens/AppointmentDetails'
+import { AppointmentCreate } from './../screens/AppointmentCreate'
+
 import { colors } from '../assets/styles/global'
 
 export const AuthRoutes = () => {
   const { Navigator, Screen } = createStackNavigator()
   return (
     <Navigator
-      initialRouteName="Home"
       headerMode="none"
       screenOptions={{
         cardStyle: {
@@ -19,6 +21,8 @@ export const AuthRoutes = () => {
     >
       <Screen name="SignIn" component={ SignIn }/>
       <Screen name="Home" component={ Home }/>
+      <Screen name="AppointmentDetails" component={ AppointmentDetails }/>
+      <Screen name="AppointmentCreate" component={ AppointmentCreate }/>
     </Navigator>
   )
 }
