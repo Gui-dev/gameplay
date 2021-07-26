935b663d87203ba94afdd072942a1dc78958b0a6
Gui-dev <adrianon10@gmail.com>
2021-07-26 03:32:58 -0300

    style: add height and width

--- src/components/GuildIcon/style.ts
+++ src/components/GuildIcon/style.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.Image`
-  height: 64px;
-  width: 64px;
-  margin-right: 20px;
+  height: 66px;
+  width: 62px;
   border-radius: 8px;
 `
