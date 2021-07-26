eee85414df1c806fc3a65827fb8030e134c5f1a4
Gui-dev <adrianon10@gmail.com>
2021-07-26 03:12:13 -0300

    style: add textAlignVertical

--- src/components/TextArea/style.ts
+++ src/components/TextArea/style.ts
@@ -4,6 +4,7 @@ import { colors, fonts } from '../../assets/styles/global'
 export const Container = styled.TextInput`
   font-size: 13px;
   font-family: ${fonts.text400};
+  textAlignVertical: top;
   color: ${colors.heading};
   margin-right: 4px;
   padding: 10px;
