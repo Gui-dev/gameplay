5391a61e319d00981e1c890a2bd99f164444d10c
Gui-dev <adrianon10@gmail.com>
2021-07-28 19:13:11 -0300

    feat: add discord config

---
+++ src/config/discordAuth.ts
@@ -0,0 +1,12 @@
+export const discordConfig = {
+  REDIRECT_URI: 'https://auth.expo.io/@anonymous/gameplay-80745ba6-e70a-4eef-b0b7-f49d9f3750f8',
+  SCOPE: 'identify%20email%20connections%20guilds',
+  RESPONSE_TYPE: 'token',
+  CLIENT_ID: '869465365808312321',
+  CDN_IMAGE: 'https://cdn.discordapp.com'
+}
+
+
+// https://auth.expo.io/@anonymous/gameplay-80745ba6-e70a-4eef-b0b7-f49d9f3750f8
+// https://discord.com/api/oauth2/authorize?client_id=869465365808312321&redirect_uri=
+// https%3A%2F%2Fexpo.auth.io%2F%40anonymous%2Fgameplay-bf9ea97f-dac5-bcad-49aef97418fa&response_type=code&scope=identify%20email%20connections%20guilds
