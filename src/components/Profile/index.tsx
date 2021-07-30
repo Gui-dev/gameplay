92201f5cc89a1fe57c48309aba9d7b42e3622832
Gui-dev <adrianon10@gmail.com>
2021-07-30 03:54:25 -0300

    feat: add user properties

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
+import { useAuth } from '../../hooks/useAuth'
+
 import { Avatar } from '../Avatar'
 import { Container, UserInfo, User, Greeting, Username, Message } from './style'
 
 export const Profile = () => {
+  const { user } = useAuth()
+
   return (
     <Container>
-      <Avatar urlImage="https://avatars.githubusercontent.com/u/23706132?v=4"/>
+      <Avatar urlImage={ user.avatar }/>
       <UserInfo>
         <User>
           <Greeting>Olá,</Greeting>
-          <Username>Gui Silva</Username>
+          <Username>{ user.firstName }</Username>
         </User>
         <Message>Hoje é dia de vitória</Message>
       </UserInfo>
