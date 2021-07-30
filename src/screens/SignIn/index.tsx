28a6e2569cc2d992fa6e9d0dc52c590cd204e386
Gui-dev <adrianon10@gmail.com>
2021-07-30 03:53:23 -0300

    feat: add function handleSignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { ActivityIndicator } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+
+import { useAuth } from '../../hooks/useAuth'
 
 import illustrationImg from './../../assets/images/illustration.png'
 import { ButtonIcon } from '../../components/ButtonIcon'
 
+import { colors } from '../../assets/styles/global'
 import { Background } from './../../components/Background'
 import { Container, Image, Content, Title, Subtitle } from './style'
 
 export const SignIn = () => {
   const { navigate } = useNavigation()
+  const { loading, signIn } = useAuth()
 
-  const handleSignIn = () => {
-    navigate('Home')
+  const handleSignIn = async () => {
+    try {
+      await signIn()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -23,10 +32,14 @@ export const SignIn = () => {
           <Title>Conecte-se{'\n'}e organize suas{'\n'}jogatinas</Title>
           <Subtitle>Crie grupos para jogar seus games{'\n'}favoritos com seus amigos</Subtitle>
 
-          <ButtonIcon
-            title="Entrar com Discord"
-            onPress={ handleSignIn }
-          />
+          { loading
+              ? <ActivityIndicator color={ colors.primary }/>
+              : <ButtonIcon
+                  title="Entrar com Discord"
+                  onPress={ handleSignIn }
+                />
+          }
+
         </Content>
       </Container>
     </Background>
