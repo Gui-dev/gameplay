f5d90c3940f9c12847fc91692e8bb7b594d07183
Gui-dev <adrianon10@gmail.com>
2021-08-02 02:47:41 -0300

    feat: add function loadGuilds

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { guilds } from './../../utils/guilds'
+import { api } from '../../services/api'
+
 import { ListDivider } from '../../components/ListDivider'
 import { Guild, GuildProps } from '../../components/Guild'
 
+import { Load } from '../../components/Load'
 import { Container, GuildList } from './style'
 
 type GuildsProps = {
@@ -11,22 +13,42 @@ type GuildsProps = {
 }
 
 export const Guilds = ( { handleGuildSelected }: GuildsProps ) => {
+  const [guilds, setGuilds] = useState<GuildProps[]>([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const loadGuilds = async () => {
+      setLoading(true)
+      const { data } = await api.get('/users/@me/guilds')
+      setGuilds(data)
+      setLoading(false)
+    }
+
+    loadGuilds()
+  }, [])
+
   return (
     <Container>
-      <GuildList
-        data={ guilds }
-        keyExtractor={ (item: any) => String(item.id) }
-        renderItem={({ item }: any) => {
-          return (
-            <Guild
-              data={ item }
-              onPress={ () => handleGuildSelected(item) }
+      {
+        loading
+          ? <Load />
+
+          : <GuildList
+              data={ guilds }
+              keyExtractor={ (item: any) => String(item.id) }
+              renderItem={({ item }: any) => {
+                return (
+                  <Guild
+                    data={ item }
+                    onPress={ () => handleGuildSelected(item) }
+                  />
+                )
+              }}
+              ListHeaderComponent={ () => <ListDivider isCentered /> }
+              ItemSeparatorComponent={ () => <ListDivider isCentered /> }
             />
-          )
-        }}
-        ListHeaderComponent={ () => <ListDivider isCentered /> }
-        ItemSeparatorComponent={ () => <ListDivider isCentered /> }
-      />
+      }
+
     </Container>
   )
 }
