f3924e186be476b8d84aa5e89c99901ee44f1eb3
Gui-dev <adrianon10@gmail.com>
2021-08-03 02:43:42 -0300

    feat: add function loadAppointments and component Load

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { Profile } from '../../components/Profile'
 import { ButtonAdd } from '../../components/ButtonAdd'
@@ -9,12 +10,18 @@ import { appointments } from '../../utils/appointments'
 import { Appointment } from '../../components/Appointment'
 import { ListDivider } from '../../components/ListDivider'
 
+import { COLLECTION_APPOINTMENTS } from '../../config/storage'
+import { IAppointmentProps } from './../../components/Appointment/IAppointment'
+
 import { Background } from './../../components/Background'
 import { Container, Header, ListMatch } from './style'
+import { Load } from '../../components/Load'
 
 export const Home = () => {
   const { navigate } = useNavigation()
   const [category, setCategory] = useState('')
+  const [appointments, setAppointments] = useState<IAppointmentProps[]>([])
+  const [loading, setLoading] = useState(false)
 
   const handleCategorySelect = (categoryId: string) => {
     categoryId === category ? setCategory('') : setCategory(categoryId)
@@ -27,6 +34,23 @@ export const Home = () => {
   const handleNavigationAppointmentDetails = () => {
     navigate('AppointmentDetails')
   }
+
+  useFocusEffect(useCallback(() => {
+    const loadAppointments = async () => {
+      setLoading(true)
+      const storageResponse = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
+      const storage: IAppointmentProps[] = storageResponse ? JSON.parse(storageResponse) : []
+      if (category) {
+        setAppointments(storage.filter(appointment => appointment.category === category))
+      } else {
+        setAppointments(storage)
+      }
+      setLoading(false)
+    }
+
+    loadAppointments()
+  }, [category])
+  )
 
   return (
     <Background>
@@ -42,24 +66,30 @@ export const Home = () => {
           hasCheckBox={ !!category }
         />
 
-        <ListHeader
-          title="Partidas agendadas"
-          subtitle="Total 6"
-        />
-
-        <ListMatch
-          data={ appointments }
-          keyExtractor={ (item: any) => item.id }
-          renderItem={ ({ item }: any) => {
-            return (
-              <Appointment
-                data={ item }
-                onPress={ handleNavigationAppointmentDetails }
-              />
-            )
-          } }
-          ItemSeparatorComponent={ () => <ListDivider /> }
-        />
+          {
+            loading
+              ? <Load />
+              :
+                <>
+                  <ListHeader
+                    title="Partidas agendadas"
+                    subtitle={`Total ${appointments.length}`}
+                  />
+                  <ListMatch
+                    data={ appointments }
+                    keyExtractor={ (item: any) => item.id }
+                    renderItem={ ({ item }: any) => {
+                      return (
+                        <Appointment
+                          data={ item }
+                          onPress={ handleNavigationAppointmentDetails }
+                        />
+                      )
+                    } }
+                    ItemSeparatorComponent={ () => <ListDivider /> }
+                  />
+                </>
+          }
       </Container>
     </Background>
   )
