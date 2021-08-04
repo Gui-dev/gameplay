7c8a067ba6bce53640d6b50fcda019e6d3ae9715
Gui-dev <adrianon10@gmail.com>
2021-08-04 03:03:49 -0300

    feat: add seeking guild and add function handleShareInvitation

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Alert, Platform, Share } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 import { Fontisto } from '@expo/vector-icons'
+import * as Linking from 'expo-linking'
 
 import { Header } from '../../components/Header'
 import { ListHeader } from '../../components/ListHeader'
@@ -7,19 +10,61 @@ import { Member } from '../../components/Member'
 import { ListDivider } from '../../components/ListDivider'
 import { ButtonIcon } from '../../components/ButtonIcon'
 
-import { members } from '../../utils/members'
+import { IAppointmentProps } from './../../components/Appointment/IAppointment'
+import { MemberProps } from '../../components/Member/IMemberProps'
+import { api } from '../../services/api'
 
 import bannerImg from './../../assets/images/banner.png'
 import { Background } from '../../components/Background'
+import { Load } from '../../components/Load'
 import { colors } from '../../assets/styles/global'
 import { Container, ActionButton, ImageBackground, BannerContent, Title, Subtitle, MembersList,
   ButtonContainer
 } from './style'
 
-export const AppointmentDetails = () => {
+type GuildWidget = {
+  id: string
+  name: string
+  instant_invite: string
+  members: MemberProps[]
+  presence_count: number
+}
 
-  const handleShare = () => {
-    console.log('Here')
+type RouteParams = {
+  guildSelected: IAppointmentProps
+}
+
+export const AppointmentDetails = () => {
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget)
+  const [loading, setLoading] = useState(true)
+  const route = useRoute()
+  const { guildSelected } = route.params as RouteParams
+
+
+  useEffect(() => {
+    const guildWidget = async () => {
+      try {
+        const { data } = await api.get(`/guilds/${guildSelected.guild.id}/widget.json`)
+        setWidget(data)
+      } catch (error) {
+        Alert.alert('Oooops', 'Verifique as configurações do servidor, Será que o Widget está habilitado')
+      } finally {
+        setLoading(false)
+      }
+    }
+    guildWidget()
+  }, [])
+
+  const handleShareInvitation = () => {
+    const message = Platform.OS === 'ios'
+      ? `Junte-se a ${guildSelected.guild.name}`
+      : widget.instant_invite
+
+    Share.share({
+      title: 'Compartilhar',
+      message,
+      url: widget.instant_invite
+    })
   }
 
   return (
@@ -28,8 +73,9 @@ export const AppointmentDetails = () => {
         <Header
           title="Detalhes"
           action={
+            guildSelected.guild.owner &&
             <ActionButton
-              onPress={ handleShare }
+              onPress={ handleShareInvitation }
             >
               <Fontisto name="share" size={24} color={colors.primary}/>
             </ActionButton>
@@ -38,26 +84,33 @@ export const AppointmentDetails = () => {
 
         <ImageBackground source={ bannerImg }>
           <BannerContent>
-            <Title>Lendários</Title>
-            <Subtitle>É hoje que vamos chegar ao challenger sem perder uma partida da md10</Subtitle>
+            <Title>{ guildSelected.guild.name }</Title>
+            <Subtitle>{ guildSelected.description }</Subtitle>
           </BannerContent>
         </ImageBackground>
 
-        <ListHeader
-          title="Jogadores"
-          subtitle="Total 3"
-        />
 
-        <MembersList
-          data={ members }
-          keyExtractor={ (item: any) => String(item.id) }
-          renderItem={({ item }: any) => {
-            return (
-              <Member data={ item }/>
-            )
-          }}
-          ItemSeparatorComponent={ () => <ListDivider isCentered/> }
-        />
+        {
+          loading
+           ? <Load />
+           : <>
+              <ListHeader
+                title="Jogadores"
+                subtitle={`Total ${widget.presence_count}`}
+              />
+
+              <MembersList
+                data={ widget.members }
+                keyExtractor={ (item: any) => String(item.id) }
+                renderItem={({ item }: any) => {
+                  return (
+                    <Member data={ item }/>
+                  )
+                }}
+                ItemSeparatorComponent={ () => <ListDivider isCentered/> }
+              />
+             </>
+        }
 
         <ButtonContainer>
           <ButtonIcon
